e5921ec2aea3a69f2b98e2e4d6a5581943470813
HowieHepburn <2652740017@qq.com>
2023-05-28 17:02:24 +0800

    增加path_listenner节点绘图功能

--- gazebo_urdf_demo_pkg/scripts/path_listener.py
+++ gazebo_urdf_demo_pkg/scripts/path_listener.py
@@ -6,6 +6,11 @@ from nav_msgs.msg import Path
 from std_msgs.msg import Header
 from move_base_msgs.msg import MoveBaseActionGoal
 from scipy.integrate import simps
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
+# import matplotlib.pyplot as plt
 
 class ActualPathLengthCalculator:
 
@@ -17,21 +22,24 @@ class ActualPathLengthCalculator:
         # 订阅全局路径规划结果
         rospy.Subscriber('/move_base/GlobalPlanner/plan', Path, self.gpath_callback)
         # 订阅目标位置信息
-        rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, self.goal_callback)
-
+        rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, self.goal_callback) 
+       
         self.goal_pose = None
         self.path_length = 0.0
         self.start_time = None
 
         self.path = None
         
-        self.flag = 0
-        self.flag2 = 0
-        self.goal_tolorance = 0.1
-        self.positions = []
-
+        self.flag = False
+        self.flag2 = False
+        self.goal_tolorance = 0.5
+        self.ac_positions = []
+        # 创建一个Figure对象和一个子图
+        self.fig, self.ax = plt.subplots()
+        
     def gpath_callback(self, msg):
         if self.flag2:
+            self.flag2 = False
             # 计算路径规划的时间
             rospy.loginfo("Global Plan Cost time: %f s",(rospy.get_rostime() - self.start_time).to_sec())
             self.path = msg
@@ -39,37 +47,44 @@ class ActualPathLengthCalculator:
             poses = [pose.pose.position for pose in self.path.poses]
             gx = [pose.x for pose in poses]
             gy = [pose.y for pose in poses]
-            
+            # 画出全局路径规划曲线
+            self.ax.plot(gx,gy,'b',label="Global Path")
             # 计算全局路径的长度
             global_path_lenth = self.calculate_path_length(gx,gy)
             
             rospy.loginfo('Global Path lenth: %f m', global_path_lenth)
-            self.flag2 = 0
+            
     
     def odom_callback(self, msg):
         # 获取机器人当前位姿
         position = msg.pose.pose.position
-        self.positions.append(position)
+        self.ac_positions.append(position)
 
         # if self.goal_pose is not None:
         #     rospy.loginfo("Distance %f xn:%f yn:%f xg:%f yg:%f",self.distance_2d(position, self.goal_pose),position.x,position.y,self.goal_pose.x,self.goal_pose.y)
         
-        if self.goal_pose is not None:
+        if self.goal_pose is not None and self.flag:
             a = self.distance_2d(position, self.goal_pose)
-            # rospy.loginfo("distance %f m", a)
-        # 判断是否到达目标点
+            # rospy.loginfo("distance %f m", a) # 打印出距离目标点位置
+            # 判断是否到达目标点
             if a <= self.goal_tolorance:
                 rospy.logwarn('Reached the goal.')
+                self.flag = False
                 #拟合曲线
-                x = [pose.x for pose in self.positions]
-                y = [pose.y for pose in self.positions]
-
+                x = [pose.x for pose in self.ac_positions]
+                y = [pose.y for pose in self.ac_positions]
+                #画出实际轨迹
+                self.ax.plot(x, y, 'r', label='Actual Path')
+                self.ax.legend()
+                self.fig.canvas.draw()
+                self.fig.savefig('/home/howie/catkin_ws/picture.png')
+                #计算路径长度并打印
                 path_length = self.calculate_path_length(x, y)
                 rospy.loginfo('Cost lenth: %f m', path_length)
                 rospy.loginfo("Cost time: %f s",(rospy.get_rostime() - self.start_time).to_sec())
                 # 清空位置列表
-                self.positions = []        
-                self.flag = 0
+                self.ac_positions = []        
+                
 
 
     def goal_callback(self, msg):
@@ -78,8 +93,8 @@ class ActualPathLengthCalculator:
         
         self.goal_pose = msg.goal.target_pose.pose.position
         self.start_time = rospy.get_rostime()
-        self.flag = 1
-        self.flag2 = 1
+        self.flag = True
+        self.flag2 = True
 
     def distance_2d(self, p1, p2):
         dx = p2.x - p1.x
@@ -96,7 +111,6 @@ class ActualPathLengthCalculator:
         return path_length
     
 if __name__ == '__main__':
-    
     try:
         calculator = ActualPathLengthCalculator()
         rospy.spin()
